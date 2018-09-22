8594d361555cf5b2ec704abe6eed0e1b4c60bc7a
Alexandra Frydl <me@alexfrydl.com>
2018-09-22 15:16:40 -0400

    Reduce number of modules in engine

--- engine/src/engine/mod.rs
+++ engine/src/engine/mod.rs
@@ -15,86 +15,32 @@
 //! initialization of an engine context. Until `engine::run` is called, the
 //! context is still in init mode and can have new systems and processes
 //! added.
-//!
-//! The `processes` module defines the `Process` trait, which describes an
-//! _engine process_, a custom part of the main engine loop.
-//!
-//! The `resources` and `components` modules support ECS.
 
 use super::*;
-pub use specs::System;
+
+pub use specs::{Component, ReadStorage, System, WriteStorage};
+
+pub use specs::shred::{
+  Fetch as FetchResource, FetchMut as FetchResourceMut, Read as ReadResource, Resource,
+  Write as WriteResource,
+};
 
 pub mod init;
-pub mod window;
 
-mod components;
-mod context;
-mod processes;
-mod resources;
-
-pub use self::components::*;
-pub use self::context::*;
-pub use self::processes::*;
-pub use self::resources::*;
-pub use self::window::*;
-
-/// Runs the engine loop until `engine::exit` is called.
-///
-/// Each iteration of the main engine loop is split into three sequential
-/// phases: early, regular, and late. In each phase, first the systems for that
-/// phase are dispatched, then processes are updated in the order they were
-/// added.
-///
-/// “Early” systems and processes implement “inputs” to the engine. For example:
-/// time, player input, or updates from a multiplayer server.
-///
-/// “Late” systems and processes implement “outputs” from the engine. For
-/// example: sending updates _to_ a multiplayer server, calculating UI layout,
-/// or drawing graphics on the screen.
-///
-/// The majority of systems and processes are neither early nor late, and they
-/// implement most game logic.
-///
-/// Systems are run in parallel in each of the three phases. After the systems
-/// run in each phase, all processes are updated in sequence.
-pub fn run(ctx: &mut Context) {
-  let setup = ctx
-    .init_state
-    .take()
-    .expect("engine context is already running");
-
-  let mut processes = setup.processes;
-  let mut early_systems = setup.early_systems.build();
-  let mut systems = setup.systems.build();
-  let mut late_systems = setup.late_systems.build();
-
-  // Run the engine loop.
-  while !ctx.exiting {
-    // Update the window each loop if there is one.
-    if ctx.window_handle.borrow().is_some() {
-      update_window(ctx);
-    }
-
-    // Run all systems and processes.
-    early_systems.dispatch(&mut ctx.world.res);
-
-    for process in &mut processes {
-      process.early_update(ctx);
-    }
-
-    systems.dispatch(&mut ctx.world.res);
-
-    for process in &mut processes {
-      process.update(ctx);
-    }
-
-    late_systems.dispatch(&mut ctx.world.res);
-
-    for process in &mut processes {
-      process.late_update(ctx);
-    }
-  }
+pub mod storages {
+  pub use specs::storage::{
+    BTreeStorage, DenseVecStorage, FlaggedStorage, HashMapStorage, NullStorage, ReadStorage,
+    WriteStorage,
+  };
 }
+
+mod context;
+mod running;
+mod window;
+
+pub use self::context::*;
+pub use self::running::*;
+pub use self::window::*;
 
 /// Creates a new entity builder that will build an entity in the engine
 /// context.
@@ -102,7 +48,44 @@ pub fn create_entity<'a>(ctx: &'a mut Context) -> EntityBuilder<'a> {
   ctx.world.create_entity()
 }
 
-/// Exits the engine loop started with `engine::run`.
-pub fn exit(ctx: &mut Context) {
-  ctx.exiting = true;
+/// Adds a resource to the engine context.
+pub fn add_resource(ctx: &mut Context, resource: impl Resource) {
+  ctx.world.add_resource(resource)
+}
+
+/// Fetches a resource from the engine context.
+pub fn fetch_resource<'a, T: Resource + Send + 'a>(ctx: &'a Context) -> FetchResource<'a, T> {
+  ctx.world.read_resource::<T>()
+}
+
+/// Mutably fetches a resource from the engine context.
+pub fn fetch_resource_mut<'a, T: Resource + Send + 'a>(
+  ctx: &'a Context,
+) -> FetchResourceMut<'a, T> {
+  ctx.world.write_resource::<T>()
+}
+
+/// Checks whether the engine context has a resource of type `T`.
+pub fn has_resource<T: Resource + Send>(ctx: &Context) -> bool {
+  ctx.world.res.has_value::<T>()
+}
+
+/// Adds storage for components of type `T` to the engine context.
+pub fn add_storage<T>(ctx: &mut Context)
+where
+  T: Component,
+  T::Storage: Default,
+{
+  ctx.world.register::<T>();
+}
+
+/// Fetches the storage for components of type `T` to the engine context.
+pub fn fetch_storage<'a, T: Component>(ctx: &'a Context) -> ReadStorage<'a, T> {
+  ctx.world.read_storage::<T>()
+}
+
+/// Mutably fetches the storage for components of type `T` to the engine
+/// context.
+pub fn fetch_storage_mut<'a, T: Component>(ctx: &'a Context) -> WriteStorage<'a, T> {
+  ctx.world.write_storage::<T>()
 }
---
+++ engine/src/engine/running.rs
@@ -0,0 +1,79 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use super::*;
+
+/// Trait describing a _process_ that is updated during each iteration of an
+/// engine loop.
+pub trait Process {
+  /// Invoked after early systems are dispatched.
+  fn early_update(&mut self, _ctx: &mut Context) {}
+  /// Invoked after systems are dispatched.
+  fn update(&mut self, _ctx: &mut Context) {}
+  /// Invoked after late systems are dispatched.
+  fn late_update(&mut self, _ctx: &mut Context) {}
+}
+
+/// Runs the engine loop until `engine::exit` is called.
+///
+/// Each iteration of the main engine loop is split into three sequential
+/// phases: early, regular, and late. In each phase, first the systems for that
+/// phase are dispatched, then processes are updated in the order they were
+/// added.
+///
+/// “Early” systems and processes implement “inputs” to the engine. For example:
+/// time, player input, or updates from a multiplayer server.
+///
+/// “Late” systems and processes implement “outputs” from the engine. For
+/// example: sending updates _to_ a multiplayer server, calculating UI layout,
+/// or drawing graphics on the screen.
+///
+/// The majority of systems and processes are neither early nor late, and they
+/// implement most game logic.
+///
+/// Systems are run in parallel in each of the three phases. After the systems
+/// run in each phase, all processes are updated in sequence.
+pub fn run(ctx: &mut Context) {
+  let init_state = ctx
+    .init_state
+    .take()
+    .expect("engine context is already running");
+
+  let mut processes = init_state.processes;
+  let mut early_systems = init_state.early_systems.build();
+  let mut systems = init_state.systems.build();
+  let mut late_systems = init_state.late_systems.build();
+
+  // Run the engine loop.
+  while !ctx.exiting {
+    // Update the window each loop if there is one.
+    if ctx.window_handle.borrow().is_some() {
+      update_window(ctx);
+    }
+
+    // Run all systems and processes.
+    early_systems.dispatch(&mut ctx.world.res);
+
+    for process in &mut processes {
+      process.early_update(ctx);
+    }
+
+    systems.dispatch(&mut ctx.world.res);
+
+    for process in &mut processes {
+      process.update(ctx);
+    }
+
+    late_systems.dispatch(&mut ctx.world.res);
+
+    for process in &mut processes {
+      process.late_update(ctx);
+    }
+  }
+}
+
+/// Exits the engine loop started with `engine::run`.
+pub fn exit(ctx: &mut Context) {
+  ctx.exiting = true;
+}
