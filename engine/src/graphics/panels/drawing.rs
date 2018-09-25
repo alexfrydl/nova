55f7a303b26c32a257dfff862a02d123ca18d96c
Alexandra Frydl <me@alexfrydl.com>
2018-09-25 16:49:05 -0400

    Remove unused imports

--- engine/src/graphics/panels/drawing.rs
+++ engine/src/graphics/panels/drawing.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use super::{Hierarchy, Layout, Root};
+use super::{Hierarchy, Layout};
 use crate::graphics::{Canvas, Color, DrawParams, Image};
 use crate::prelude::*;
 use std::sync::{Arc, Mutex};
