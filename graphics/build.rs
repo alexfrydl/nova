753a333d810584df10f1c9b8215f43eccb657e59
Alexandra Frydl <me@alexfrydl.com>
2018-11-01 21:56:51 -0400

    Improve shaders

--- graphics/build.rs
+++ graphics/build.rs
@@ -3,18 +3,17 @@ use std::error::Error;
 fn main() -> Result<(), Box<Error>> {
   use glsl_to_spirv::ShaderType;
 
-  // Tell the build script to only run again if the source shaders change.
-  println!("cargo:rerun-if-changed=src/shaders/hlsl");
-
-  // Create destination path if necessary
   std::fs::create_dir_all("src/shaders/spirv")?;
 
-  // Build each shader in the path.
+  println!("cargo:rerun-if-changed=src/shaders/hlsl");
+
   for entry in std::fs::read_dir("src/shaders/hlsl")? {
     let entry = entry?;
 
     if entry.file_type()?.is_file() {
       let in_path = entry.path();
+
+      println!("cargo:rerun-if-changed={}", entry.path().to_str().unwrap());
 
       // Support only vertex and fragment shaders currently
       let shader_type = in_path
--- graphics/src/shaders/hlsl/default.frag
+++ graphics/src/shaders/hlsl/default.frag
@@ -1,8 +1,8 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 
-layout(location = 0) out vec4 target;
+layout(location = 0) out vec4 outColor;
 
 void main() {
-  target = vec4(0.5, 0.5, 1.0, 1.0);
+  outColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
