71a4eb98f0c0ecfd2f22d7cdaa144d7dc9b1d5e5
Alexandra Frydl <me@alexfrydl.com>
2019-02-25 18:17:17 -0500

    Expose derive macros for shred

--- nova/src/ecs.rs
+++ nova/src/ecs.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+pub mod derive;
+
 use crate::engine;
 
 pub use specs::join::{Join, ParJoin};
